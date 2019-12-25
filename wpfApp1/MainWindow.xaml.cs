0b09ffdb2ee8fd03b1690b07d324ee368b709d87
beiteli <lbt12345@126.com>
2019-12-25 11:32:51 +0800

    submit

---
+++ wpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _Model = new MyModel();
+            this.DataContext = _Model;
+        }
+        private MyModel _Model;
+        private void OnShowData_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(_Model.Data);
+        }
+
+        private void OnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            _Model.ClearData();
+        }
+        private void OnOpen_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ wpfApp1/MyModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WpfApp1
+{
+    class MyModel : INotifyPropertyChanged
+    {
+        public string Data
+        {
+            get => _Data;
+            set
+            {
+                if (value == Data)
+                    return;
+                _Data = value;
+                OnPropertyChanged(nameof(Data));
+            }
+        }
+        private string _Data = "My Data!";
+
+        public void ClearData()
+        {
+            Data = "";
+        }
+        private void OnPropertyChanged(string aPropertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
